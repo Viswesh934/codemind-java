fd920997da4ae48dc2bdacb6e147febb4446124e
Viswesh934 <98519767+Viswesh934@users.noreply.github.com>
2022-09-22 22:22:11 +0530

    import java.util.Scanner; class hey{     public static void main(String args[]){     Scanner o=new Scanner(System.in);     float c=o.nextFloat();     float p=c*9;     float t= p/5;     double f=32+t;     System.out.printf("%.2f",f);} }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class hey{
+    public static void main(String args[]){
+    Scanner o=new Scanner(System.in);
+    float c=o.nextFloat();
+    float p=c*9;
+    float t= p/5;
+    double f=32+t;
+    System.out.printf("%.2f",f);}
+}
