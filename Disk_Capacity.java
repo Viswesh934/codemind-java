cabb101046b9dab5b3ef3d80ed69fcd6380e3c94
Viswesh934 <98519767+Viswesh934@users.noreply.github.com>
2022-09-25 22:20:21 +0530

    import java.util.*; class me{        public static void main(String []args){         Scanner n=new Scanner(System.in);         int a=n.nextInt();         int b=n.nextInt();         int c=n.nextInt();         int r=512;         System.out.println(a*b*c*r*2);
        }
    }

---
+++ Disk_Capacity.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class me{
+       public static void main(String []args){
+        Scanner n=new Scanner(System.in);
+        int a=n.nextInt();
+        int b=n.nextInt();
+        int c=n.nextInt();
+        int r=512;
+        System.out.println(a*b*c*r*2);
+        
+    }
+}
