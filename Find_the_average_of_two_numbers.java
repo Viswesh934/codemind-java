7690520fa33779cd145389ef3cbaa4d24921f361
Viswesh934 <98519767+Viswesh934@users.noreply.github.com>
2022-09-25 21:39:03 +0530

    import java.io.*; import java.lang.*; import java.util.*; class area{     public static void main(String args[]){         Scanner n= new Scanner(System.in);         float a=n.nextFloat();         float b=n.nextFloat();         double s;         s=(a+b)/2;         System.out.printf("%.4f",s);
        }
    }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+class area{
+    public static void main(String args[]){
+        Scanner n= new Scanner(System.in);
+        float a=n.nextFloat();
+        float b=n.nextFloat();
+        double s;
+        s=(a+b)/2;
+        System.out.printf("%.4f",s);
+        
+        
+    }
+}
