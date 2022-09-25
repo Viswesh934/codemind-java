adc3dc24f09217c1890a234f0d6c07aebf4d6208
Viswesh934 <98519767+Viswesh934@users.noreply.github.com>
2022-09-25 22:00:55 +0530

    import java.util.*; class me{        public static void main(String []args){         Scanner n=new Scanner(System.in);         int a=n.nextInt();         int b=n.nextInt();         System.out.println(a*b);
        }
    }

---
+++ Multiplication.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class me{
+       public static void main(String []args){
+        Scanner n=new Scanner(System.in);
+        int a=n.nextInt();
+        int b=n.nextInt();
+        System.out.println(a*b);
+        
+    }
+}
