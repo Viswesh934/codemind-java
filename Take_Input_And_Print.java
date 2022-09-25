138ab5f0c76fa993880d11a8fb965e2130365bf2
Viswesh934 <98519767+Viswesh934@users.noreply.github.com>
2022-09-25 22:10:28 +0530

    import java.util.*; class me{        public static void main(String []args){         Scanner n=new Scanner(System.in);         String a=n.nextLine();
            System.out.println(a);
            
        }
    }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class me{
+       public static void main(String []args){
+        Scanner n=new Scanner(System.in);
+        String a=n.nextLine();
+    
+        System.out.println(a);
+        
+    }
+}
