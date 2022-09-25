b6215c29c2b099044cbe5211a27845a1654f08e7
Viswesh934 <98519767+Viswesh934@users.noreply.github.com>
2022-09-25 21:58:51 +0530

    import java.io.*; import java.util.*; class me{        public static void main(String []args){         Scanner n=new Scanner(System.in);         float a=n.nextFloat();         float b=n.nextFloat();         float r=a*b;         System.out.printf("%.2f",r);
        }
    }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class me{
+       public static void main(String []args){
+        Scanner n=new Scanner(System.in);
+        float a=n.nextFloat();
+        float b=n.nextFloat();
+        float r=a*b;
+        System.out.printf("%.2f",r);
+        
+        
+    }
+}
