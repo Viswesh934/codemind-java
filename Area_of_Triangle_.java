bb0fe0d9aa521c9320707ca0d247ffdb1c4ba678
Viswesh934 <98519767+Viswesh934@users.noreply.github.com>
2022-09-25 21:32:07 +0530

    import java.io.*; import java.lang.*; import java.util.Scanner; class area{     public static void main(String args[]){         Scanner n= new Scanner(System.in);         float a=n.nextFloat();         float b=n.nextFloat();         float c=n.nextFloat();         float s;         s=(a+b+c)/2;          if (a < 0 || b < 0 || c <0 || (a+b <= c) ||             a+c <=b || b+c <=a)         {             System.out.println("Not a valid triangle");             System.exit(0);         }
            else{
            double ar;
            ar=Math.sqrt(s*(s-a)*(s-b)*(s-c));
            System.out.println(String.format("%.2f",ar));}
            
        }
    }

---
+++ Area_of_Triangle_.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.lang.*;
+import java.util.Scanner;
+class area{
+    public static void main(String args[]){
+        Scanner n= new Scanner(System.in);
+        float a=n.nextFloat();
+        float b=n.nextFloat();
+        float c=n.nextFloat();
+        float s;
+        s=(a+b+c)/2;
+         if (a < 0 || b < 0 || c <0 || (a+b <= c) ||
+            a+c <=b || b+c <=a)
+        {
+            System.out.println("Not a valid triangle");
+            System.exit(0);
+        }
+        
+        else{
+        double ar;
+        ar=Math.sqrt(s*(s-a)*(s-b)*(s-c));
+        System.out.println(String.format("%.2f",ar));}
+        
+    }
+}
