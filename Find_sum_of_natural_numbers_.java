c2b70d80e73803e02975c9c87b31772b7ff45239
Viswesh934 <98519767+Viswesh934@users.noreply.github.com>
2023-02-05 15:06:38 +0530

    import java.util.Scanner; class prime {     public static void main(String rags[])     {         Scanner sc=new Scanner(System.in);         int a,b;         a=sc.nextInt();         b=(a*(a+1))/2;         System.out.print(b);     } }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class prime
+{
+    public static void main(String rags[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        a=sc.nextInt();
+        b=(a*(a+1))/2;
+        System.out.print(b);
+    }
+}
